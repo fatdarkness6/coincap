d890fbbba2e43dc59d34a9ca6b0b70791f68bcad
fatdarkness3 <steamarsam70@gmail.com>
2024-06-24 12:33:33 +0330

    add chart and ...

--- components/pages/homePage/homePage.jsx
+++ components/pages/homePage/homePage.jsx
@@ -1,99 +1,121 @@
 import { useEffect, useState } from 'react';
 import { homePageApi } from '../../../api/homePageApi';
-import Header from "../../header/header";
-import RenderCoins from "./renderCoins"
+import Header from '../../header/header';
+import RenderCoins from './renderCoins';
+import { iconApi } from '../../../api/iconsApi';
+import axios from 'axios';
 
 export default function HomePage() {
   let [plus, updatePlus] = useState(20);
   let [state, updateState] = useState(false);
   let [res, updateRes] = useState([]);
-  let [upd , setUpd] = useState(1)
-
-  
+  let [upd, setUpd] = useState(1);
+  let [itsTrue, setItsTrue] = useState('');
+  let [itsFalse, setItsFalse] = useState(false);
   useEffect(() => {
-    
+    homePageApi(plus)
+      .then((e) => {
+        setItsTrue(e.status);
+        return e.json();
+      })
+      .then((e) => {
+        updateRes(e.data);
+      })
+      .catch(() => {
+        return <h1>error</h1>;
+      });
+  }, [plus, upd]);
 
-      homePageApi(plus)
-        .then((e) => {
-          return e.json();
-        })
-        .then((e) => {
-          updateRes(e.data);
-        }).catch(() => {
-          return <h1>error</h1>
-        })
-   
-  }, [plus , upd]);
-useEffect(() => {
+  useEffect(() => {
+    setInterval(() => {
+      setUpd(upd++);
+    }, 40000);
+  }, []);
 
-  setInterval(() => {
-    setUpd(upd++)
-  },40000)
-} , [])
   if (state) {
     setTimeout(() => {
       updateState(false);
     }, 1000);
   }
-  console.log(upd)
   return (
     <>
       <div className='father'>
         <div className='homePage-part1'>
-            <Header/>
+          <Header />
         </div>
-        <div className='homePage-part2'>
-
-        </div>
+        <div className='homePage-part2'></div>
         <div className='homePage-part3'>
-        <div className='header-wrapper'>
-          <table className='homePage-container'>
-          <thead>
-            <tr className='part1'>
-              
-                {/* <li className='section1'> */}
-               
-               <th><h6>Rank</h6></th> 
-                
-                  
-                <th className='left align'><h6>Name</h6></th>  
-                {/* </li> */}
-                {/* <li className='section2'> */}
-                 <th className='right align'> <h6>Price</h6></th>
-                  <th className='right align'><h6>Market Cap</h6></th>
-                 <th className='right align'><h6>VWAP(24Hr)</h6></th> 
-                  <th className='right align'><h6>Supply</h6></th>
-                  <th className='right align'><h6>Volume(24Hr)</h6></th>
-                 <th className='right align'><h6>Change(24Hr)</h6></th> 
-                
-                {/* </li> */}
-              
-            </tr>
-            </thead>
-            <tbody>
-            {res.map(e => {
-            console.log(e)
-            return <RenderCoins name = {e.name} id = {e.rank} priceUsd = {e.priceUsd} marketCapUsd = {e.marketCapUsd} vwap24Hr = {e.vwap24Hr} supply = {e.supply} volumeUsd24Hr = {e.volumeUsd24Hr} changePercent24Hr = {e.changePercent24Hr} symbol= {e.symbol}/>
-          })}
-            </tbody>
-            
-          </table>
-        </div>
-        
-          
+          <div className='header-wrapper'>
+            <table className='homePage-container'>
+              <thead>
+                <tr className='part1'>
+                  {/* <li className='section1'> */}
+
+                  <th>
+                    <h6>Rank</h6>
+                  </th>
+
+                  <th className='left align'>
+                    <h6>Name</h6>
+                  </th>
+                  {/* </li> */}
+                  {/* <li className='section2'> */}
+                  <th className='right align'>
+                    {' '}
+                    <h6>Price</h6>
+                  </th>
+                  <th className='right align'>
+                    <h6>Market Cap</h6>
+                  </th>
+                  <th className='right align'>
+                    <h6>VWAP(24Hr)</h6>
+                  </th>
+                  <th className='right align'>
+                    <h6>Supply</h6>
+                  </th>
+                  <th className='right align'>
+                    <h6>Volume(24Hr)</h6>
+                  </th>
+                  <th className='right align'>
+                    <h6>Change(24Hr)</h6>
+                  </th>
+
+                  {/* </li> */}
+                </tr>
+              </thead>
+              <tbody>
+                {res.map((e) => {
+                  return (
+                    <RenderCoins
+                      name={e.name}
+                      id={e.rank}
+                      nameOfTheCoins={e.id}
+                      priceUsd={e.priceUsd}
+                      marketCapUsd={e.marketCapUsd}
+                      vwap24Hr={e.vwap24Hr}
+                      supply={e.supply}
+                      volumeUsd24Hr={e.volumeUsd24Hr}
+                      changePercent24Hr={e.changePercent24Hr}
+                      symbol={e.symbol}
+                    />
+                  );
+                })}
+              </tbody>
+            </table>
+          </div>
         </div>
         <div className='homePage-part4'>
-        {!state ? (
+          {res.length == plus ? (
             <button
+              className='load-button'
               onClick={() => {
                 updateState(true);
                 updatePlus((plus += 20));
-                console.log(plus);
               }}>
-              show more
+              View More
             </button>
           ) : (
-            <button>please wait</button>
+            <button className='load-button'>please wait</button>
           )}
         </div>
       </div>
--- components/pages/homePage/renderCoins.jsx
+++ components/pages/homePage/renderCoins.jsx
@@ -1,83 +1,251 @@
-import { useEffect, useRef, useState } from "react";
-import LineChart from "../../chart/line";
-
+import { useEffect, useRef, useState } from 'react';
+import React, { Component } from 'react';
+import { Line } from 'react-chartjs-2';
+import { candleApi } from '../../../api//candle';
+import Moment from 'react-moment';
+import moment from 'moment';
+import {
+  Chart as ChartJs,
+  LineElement,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+ChartJs.register(
+  LineElement,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  Tooltip,
+  Legend
+);
 
 export default function RenderCoins(props) {
-    let [tr , setTrue] = useState(false)
+  const start = moment().add(-10, 'm');
+  let [tr, setTrue] = useState(false);
+  let [response, setResponse] = useState([]);
+  let [click, setClick] = useState(false);
+  let [upd  , setUpd] = useState(1);
+  //....................................chart...............................
 
-    function calculate(x) {
+  useEffect(() => {
+    let name = props.nameOfTheCoins;
+    if (click) {
 
-        if (x >= 0.01) {
-            return "$" + parseFloat(x).toFixed(2)
-        }else if (x < 0.01) {
-            return "$" + parseFloat(x).toFixed(6)
-        }
+        candleApi(name)
+          .then((e) => {
+            return e.json();
+          })
+          .then((e) => {
+            setResponse(e.data);
+          });
     }
+  }, [click , upd]);
 
-    function calculateBigNumber(y) {
-         if (y <= 1000000000000 && y >=100000000000) {
-            let transformed = `${(y / 1000000000).toFixed(2)}b`;
-            return transformed;
-        }else if(y <= 100000000000 && y>= 10000000000) {
-            return `${(y / 1000000000).toFixed(2)}b`
-        }else if (y < 10000000000 && y>= 1000000000) {
-            return `${(y / 1000000000).toFixed(2)}b`
-        }else if (y <1000000000 && y >= 100000000) {
-            return `${(y / 1000000).toFixed(2)}m`
-        }else if (y <100000000 && y >= 10000000) {
-             return `${(y / 1000000).toFixed(2)}m`
-        }else if (y <10000000 && y >= 1000000) {
-             return `${(y / 1000000).toFixed(2)}m`
-        }else if (y <10000000 && y>=100000){
-            return `${(y / 1000).toFixed(2)}k`
-        }else if (y <100000 && y >= 10000) {
-            return `${(y / 1000).toFixed(2)}k`
-        }else if (y < 10000 && y>= 1000) {
-            return `${(y / 1000).toFixed(2)}k`
-        }else if (y>=1000000000000) {
-            return  `${(y / 1000000000000).toFixed(2)}t`
-        }else if (y >= 0.01 || y < 0) {
-            return parseFloat(y).toFixed(2)
-        }
-        
+  useEffect(() => {
+    setInterval(() => {
+      setUpd(upd++);
+    }, 40000);
+  } , [])
+  console.log(response);
+  let data = {
+  labels: response.reverse().splice(0 , 250).reverse().map((e) => {
+    console.log(e);
+        return moment(e.date).format(" (DD) ( HH:mm)");
+      }),
+  
+    datasets: [
+      {
+        data: response.map((e) => {
+          return e.priceUsd;
+        }),
+        backgroundColor: 'rgba(255, 99, 132, 0.5)',
+        borderColor: 'rgba(255, 99, 132, 1)',
+      },
+    ],
+  };
+
+  let options = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: 'top',
+      },
+      title: {
+        display: true,
+        text: 'Candle Stick Chart',
+      },
+    },
+  };
+
+  //........................................................................
+
+  let lowerCase = props.symbol.toLowerCase();
+  useEffect(() => {}, []);
+
+  function calculate(x) {
+    if (x >= 0.01) {
+      return '$' + parseFloat(x).toFixed(2);
+    } else if (x < 0.01) {
+      return '$' + parseFloat(x).toFixed(6);
     }
-    useEffect(() => {
-        if (props.changePercent24Hr < 0) {
-            setTrue(true)
-        }
-    } , [])
+  }
 
-    return (
-        <>
-        <tr>
-            {/* <div className="border"> */}
+  function calculateBigNumber(y) {
+    if (y <= 1000000000000 && y >= 100000000000) {
+      let transformed = `${(y / 1000000000).toFixed(2)}b`;
+      return transformed;
+    } else if (y <= 100000000000 && y >= 10000000000) {
+      return `${(y / 1000000000).toFixed(2)}b`;
+    } else if (y < 10000000000 && y >= 1000000000) {
+      return `${(y / 1000000000).toFixed(2)}b`;
+    } else if (y < 1000000000 && y >= 100000000) {
+      return `${(y / 1000000).toFixed(2)}m`;
+    } else if (y < 100000000 && y >= 10000000) {
+      return `${(y / 1000000).toFixed(2)}m`;
+    } else if (y < 10000000 && y >= 1000000) {
+      return `${(y / 1000000).toFixed(2)}m`;
+    } else if (y < 10000000 && y >= 100000) {
+      return `${(y / 1000).toFixed(2)}k`;
+    } else if (y < 100000 && y >= 10000) {
+      return `${(y / 1000).toFixed(2)}k`;
+    } else if (y < 10000 && y >= 1000) {
+      return `${(y / 1000).toFixed(2)}k`;
+    } else if (y >= 1000000000000) {
+      return `${(y / 1000000000000).toFixed(2)}t`;
+    } else if (y >= 0.01 || y < 0) {
+      return parseFloat(y).toFixed(2);
+    } else if (y < 0.01) {
+      return parseFloat(y).toFixed(6);
+    }
+  }
 
-            {/* <div className="section1"> */}
-               <td className="center align"><h5>{props.id}</h5></td> 
-               <td><div className="mix">
-                <h5>{props.name}</h5>
-                <h5>{props.symbol}</h5>
-                </div></td>
-                
-            {/* </div> */}
-            {/* <div className="section2"> */}
-                <td className='right align'><h5>{calculate(props.priceUsd)}</h5></td>
-                <td className='right align'><h5>{calculateBigNumber(props.marketCapUsd)}</h5></td>
-                <td className='right align'><h5>{calculate(props.vwap24Hr)}</h5></td>
-                <td className='right align'><h5>{calculateBigNumber(props.supply)}</h5></td>
-                <td className='right align'><h5>{calculateBigNumber(props.volumeUsd24Hr)}</h5></td>
-                <td id={tr ? "red" : "green"} className='right align'><h5>{calculateBigNumber(props.changePercent24Hr)}%</h5></td>
-            {/* </div> */}
+  useEffect(() => {
+    if (props.changePercent24Hr < 0) {
+      setTrue(true);
+    }
+  }, []);
+  function highestValue() {
+    return Math.max.apply(
+      Math,
+      response?.map((e) => {
+        return parseFloat(e.priceUsd) >= 0.01
+          ? parseFloat(e.priceUsd).toFixed(3)
+          : parseFloat(e.priceUsd) < 0.01
+          ? parseFloat(e.priceUsd).toFixed(6)
+          : parseFloat(e.priceUsd);
+      })
+    );
+  }
+  function lowestValue() {
+    return Math.min.apply(
+      Math,
+      response?.map((e) => {
+        return parseFloat(e.priceUsd) >= 0.01
+          ? parseFloat(e.priceUsd).toFixed(3)
+          : parseFloat(e.priceUsd) < 0.01
+          ? parseFloat(e.priceUsd).toFixed(6)
+          : parseFloat(e.priceUsd);
+      })
+    );
+  }
+  function avrageValue() {
+    return (highestValue && lowestValue()) !== NaN ||
+      (highestValue && lowestValue()) !== undefined
+      ? ((highestValue() + lowestValue()) / 2).toFixed(3)
+      : 'it doesnt have value';
+  }
+  return (
+    <>
+      <tr
+        onClick={() => {
+          setClick(!click);
+        }}>
+        <td className='center align'>
+          <h5>{props.id}</h5>
+        </td>
 
-            {/* </div> */}
-        </tr>
-        {/* <tr>
-            <td></td>
-            <td>
-                
-            </td>
-        </tr> */}
-        </>
-        
-    )
+        <td className='td-flex'>
+          <img
+            src={`https://assets.coincap.io/assets/icons/${lowerCase}@2x.png`}
+          />
+          <div className='mix'>
+            <h5>{props.name}</h5>
+            <h5>{props.symbol}</h5>
+          </div>
+        </td>
+        <td className='right align'>
+          <h5>{calculate(props.priceUsd)}</h5>
+        </td>
+        <td className='right align'>
+          <h5>{calculateBigNumber(props.marketCapUsd)}</h5>
+        </td>
+        <td className='right align'>
+          <h5>{calculate(props.vwap24Hr)}</h5>
+        </td>
+        <td className='right align'>
+          <h5>{calculateBigNumber(props.supply)}</h5>
+        </td>
+        <td className='right align'>
+          <h5>{calculateBigNumber(props.volumeUsd24Hr)}</h5>
+        </td>
+        <td id={tr ? 'red' : 'green'} className='right align'>
+          <h5>{calculateBigNumber(props.changePercent24Hr)}%</h5>
+        </td>
+      </tr>
+      <tr className={click ? 'active' : 'none'}>
+        <td colSpan={11}>
+          <div className='info-part1'>
+            <div className='info-slice1'>
+              <div className='info-img'>
+                <img
+                  src={`https://assets.coincap.io/assets/icons/${lowerCase}@2x.png`}
+                />
+              </div>
+              <div className='info-mix'>
+                <div className='info-mix-slice1'>
+                  <h5>{props.name}</h5>
+                  <h5>{`(${props.symbol})`}</h5>
+                </div>
+                <div className='info-mix-slice2'>
+                  {Date().toString().slice(4, 15)}
+                </div>
+              </div>
+            </div>
+            <div className='info-slice2'>
+              <div className='info-high-low'>
+                <div className='info-high flex-align-gap-row'>
+                  <h3>HIGH</h3>
+                  {`$${highestValue()}`}
+                </div>
+                <div className='info-low flex-align-gap-row'>
+                  <h3>LOW</h3>
+                  {`$${lowestValue()}`}
+                </div>
+              </div>
+              <div className='info-avrage-change'>
+                <div className='info-high flex-align-gap-row'>
+                  <h3>AVRAGE</h3>
+                  {`$${avrageValue()}`}
+                </div>
+                <div className='info-low flex-align-gap-row'>
+                  <h3>CHANGE</h3>
+                  <div id={tr ? 'red' : 'green'}>
+                    {calculateBigNumber(props.changePercent24Hr)}%
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className='chart-part2'>
+            <Line data={data} options={options}></Line>
+          </div>
+        </td>
+      </tr>
+    </>
+  );
 }
+
+
